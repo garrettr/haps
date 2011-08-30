8025d5f529c42b8a1433fa06c506f7475777489d
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-08-30 13:22:46 -0400

    Basic file upload working like a charm!

--- quickstart.py
+++ quickstart.py
@@ -1,18 +1,34 @@
-from flask import Flask, url_for, render_template, request
+import os
+from flask import Flask, url_for, render_template, request, redirect
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    'uploads')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
 app.debug = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/upload/', methods=['POST', 'GET'])
 def upload():
     error = None
     if request.method == 'POST':
-        pass
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_thanks', filename=filename))
     else:
         return render_template('upload.html')
+
+@app.route('/upload/thanks')
+def upload_thanks(filename=None):
+    return render_template('upload_thanks.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -23,6 +39,10 @@ def login():
             return log_user_in(request.form['username'])
         else:
             error = 'Invalid username/password'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if app.debug:
     # use Flask to serve static files
