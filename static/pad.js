3df5ebc65df3c6802c17a195b623bb2b9ab437c9
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-09-03 13:03:56 -0400

    Fixed Javascript crashing bug in WebKit, with some help from mflee.

--- static/pad.js
+++ static/pad.js
@@ -4,10 +4,23 @@ function random_char() {
     return chars.charAt(rand);
 }
 function add_padding() {
+    // "loading" gif
+    document.getElementById('notes').innerHTML = '<img src="/static/loading.gif" /><br/>Please wait, generating padding...';
     var padding_size = Math.floor(Math.random()*1572864)+524288; // between .5mb and 2mb
     var padding = '';
-    for(var i=0; i<padding_size; i++) padding += random_char();
-    var padding_field = document.getElementById('padding');
-    padding_field.setAttribute('value', padding);
+
+    var size_so_far = 0;
+    var interval = setInterval(function() {
+        for(var i=0; i<10000; i++) {
+            padding += 'A';
+            size_so_far++;
+            if(size_so_far == padding_size) {
+                clearInterval(interval);
+                document.getElementById('padding').setAttribute('value', padding);
+                document.getElementById('notes').innerHTML = 'Padding generation complete!';
+                break;
+            }
+        }
+    }, 0);
 }
-window.onload=add_padding;
+window.onload=function() { add_padding(); };
