3d6b5eb9a003068f605d38e026b9fd9403210bd2
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-09-03 13:45:55 -0400

    New loading icon, niced up padding JS text.

--- static/pad.js
+++ static/pad.js
@@ -5,7 +5,7 @@ function random_char() {
 }
 function add_padding() {
     // "loading" gif
-    document.getElementById('notes').innerHTML = '<img src="/static/loading.gif" /><br/>Please wait, generating padding...';
+    document.getElementById('notes').innerHTML = '<p>Generating padding... <img src="/static/loading.gif" /></p>'
     var padding_size = Math.floor(Math.random()*1572864)+524288; // between .5mb and 2mb
     var padding = '';
 
@@ -17,7 +17,7 @@ function add_padding() {
             if(size_so_far == padding_size) {
                 clearInterval(interval);
                 document.getElementById('padding').setAttribute('value', padding);
-                document.getElementById('notes').innerHTML = 'Padding generation complete!';
+                document.getElementById('notes').innerHTML = '<p>Generating padding... complete!</p>';
                 break;
             }
         }
