02843890bffbf41d0e1a9e041161b93b3b9e779b
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-09-03 15:10:37 -0400

    Switched from += string concatenation to more efficient array join method.

--- static/pad.js
+++ static/pad.js
@@ -7,15 +7,16 @@ function add_padding() {
     // "loading" gif
     document.getElementById('notes').innerHTML = '<p>Generating padding... <img src="/static/loading.gif" /></p>'
     var padding_size = Math.floor(Math.random()*1572864)+524288; // between .5mb and 2mb
-    var padding = '';
+    var pad_list = []
 
     var size_so_far = 0;
     var interval = setInterval(function() {
         for(var i=0; i<10000; i++) {
-            padding += 'A';
+            pad_list.append('A');
             size_so_far++;
             if(size_so_far == padding_size) {
                 clearInterval(interval);
+                var padding = pad_list.join('');
                 document.getElementById('padding').setAttribute('value', padding);
                 document.getElementById('notes').innerHTML = '<p>Generating padding... complete!</p>';
                 break;
